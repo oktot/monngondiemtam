00f6234191310d46dafecea15ebcb0abc57bdc2f
Thảo Am <thaoamtanuyen@gmail.com>
2018-03-25 22:47:31 +0700

    1

--- html.js
+++ html.js
@@ -70,7 +70,7 @@ function buildGoogleAnalyticsSetup() {
 function buildGoogleAdSense() {
   const js = `
   (adsbygoogle = window.adsbygoogle || []).push({
-    google_ad_client: 'ca-pub-here',
+    google_ad_client: 'ca-pub-9122924812472590',
     enable_page_level_ads: true
   });
   `;
