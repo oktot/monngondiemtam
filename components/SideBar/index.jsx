9a16339c33d726072292f8c9c95aeb3ed187b5bc
oktot <38215780+oktot@users.noreply.github.com>
2018-05-08 17:24:30 +0700

    Update index.jsx

--- components/SideBar/index.jsx
+++ components/SideBar/index.jsx
@@ -14,8 +14,8 @@ class SideBar extends React.Component {
                 <div className='sidebar__title'>Important</div>
 		<div className='sidebar__link'>
 		<a href='/about'>Điểm Ăn Uống Bình Dương</a>
-		<a href='/cogaituoi25'>Giới thiệu về các Điểm Ăn Uống Bình Dương</a>
-		<a href='http://monngonbinhduong.net'>Những địa điểm hẹn hò tại Bình Dương</a>
+		<a href='/releases'>Giới thiệu về các quán CAFE Bình Dương</a>
+		<a href='/artists'>Những quán ăn thú vị tại Bình Dương</a>
 		<a href='/payments'>How to pay? Thanh toán</a>
 		</div>
             </div>
